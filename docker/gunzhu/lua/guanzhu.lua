6eb73b320f9c9101a543d0512e6e57297429d489
wmzhang <wmzhang@ikuai8.com>
2018-08-21 15:12:50 +0800

    第一次正式提交

--- docker/gunzhu/lua/guanzhu.lua
+++ docker/gunzhu/lua/guanzhu.lua
@@ -1,23 +1,20 @@
 local log = ngx.log
 local dict = ngx.shared.dict
 local cjson = require "cjson"
-local redis = require "resty.redis" 
+local redis = require "resty.redis-util" --https://github.com/anjia0532/lua-resty-redis-util
+local redis_cli = redis:new({ host=dict:get("redis_ip") });
+cjson.encode_empty_table_as_object(false)
 
---连接redis server
-local redis_cli = redis.new() 
-redis_cli:set_timeout(60000)
-local ok, err = redis_cli:connect(dict:get("redis_ip"), 6379) 
-if not ok then 
-    log(ngx.ERR,"connect redis error:",err)
-    return 
-end 
+--分割字符串
+function split( str,reps )
+    local resultStrList = {}
+    string.gsub(str,'[^'..reps..']+',function ( w )
+        table.insert(resultStrList,w)
+    end)
+    return resultStrList
+end
 
---local red, err = redis_cli:auth("foobared")
---if not red then
---    log(ngx.ERR,"failed to authenticate: ", err)
---    return
---end
-
+--获取http的body
 local function get_client_body()
     ngx.req.read_body()    
     local body_data = ngx.req.get_body_data()
@@ -42,60 +39,70 @@ end
 
 
 local function main()
-    --路由器查询公众号
-    local m,err = ngx.re.match(ngx.var.request_uri,"^/guanzhu/([^/]+)/weixin/mac","jo")
+    --路由器根据mac查询公众号
+    local m,err = ngx.re.match(ngx.var.request_uri,"^/guanzhu/([^/]+)/weixin/client","jo")
     local method = ngx.req.get_method()
     if m and method == "GET"  then
-        log(ngx.INFO,"received Router query message!!!")
-        local data = get_client_body()
-        if not data then
-            log(ngx.ERR,"err Router no body data")
+        log(ngx.INFO,"received query message!!!")
+        local mac = ngx.var.arg_mac
+        if not mac then
+            log(ngx.ERR,"Parameter error")
             local t = {}
             t.code = 403
-            t.status = "fail"
-            t.message = "query no body data"
-            t.guanzhu = 0
+            t.status = "error"
+            t.message = "no mac parameter"
+            t.guanzhu = {}
             ngx.send_headers()    
             ngx.print(cjson.encode(t))
             ngx.flush(true)
             return 
         end
 
-        --获取路由器上传的mac
-        local t = cjson.decode(data)
-        local mac = t.mac
-
-        --获取优先级最高的公众号
-        local resp, err = redis_cli:zrevrange(mac .. "guanzhu", 0,0)
-        if not resp then  
-            log(ngx.ERR,"err Router zrevrange") 
-            return
-        end  
-
-        if resp[1] then
-            log(ngx.INFO,"get result") 
+        --获取所有公众号
+        local guanzhus, err = redis_cli:zrevrange(mac .. "guanzhu", 0, -1)
+        if not guanzhus then  -- no key,no element
             local t = {}
             t.code = 200
-            t.messsage = "query ok!"
+            t.messsage = "no guanzhu"
             t.status = "success"
-            t.guanzhu = resp[1]
+            t.guanzhu = {}
             ngx.send_headers()    
             ngx.print(cjson.encode(t))
             ngx.flush(true) 
-            
-            --该公众号不再推送给该mac，所以删除
-            local resp, err = redis_cli:zrem(mac .. "guanzhu", resp[1])
-            if not resp then  
-                log(ngx.ERR,"err Router zrem") 
-            end
-            return 
-        end 
+            return
+        end
+
+        
+        --测试生成公众号对应的规则key是否还存在，不存在则删除公众号
+        redis_cli:init_pipeline()
+        for i = 1, #guanzhus do
+            redis_cli:exists(split(guanzhus[i],',')[1])--分割后,[1]为规则key,[2]为公众号     
+        end
+
+        local results, err = redis_cli:commit_pipeline()
+        if not results then
+            log(ngx.ERR,"failed to commit the pipelined requests: ", err)
+            return
+        end
         
         local t = {}
+        t.guanzhu = {}
+
+        redis_cli:init_pipeline()
+        for i, res in ipairs(results) do
+            if type(res) == "number" then
+                if res == 1 then
+                    table.insert(t.guanzhu,split(guanzhus[i],',')[2])
+                else
+                    redis_cli:zrem(guanzhus[i])
+                end
+            end
+        end
+        redis_cli:commit_pipeline()
+        
         t.code = 200
-        t.messsage = "no guanzhu"
+        t.messsage = "ok"
         t.status = "success"
-        t.guanzhu = 0
         ngx.send_headers()    
         ngx.print(cjson.encode(t))
         ngx.flush(true) 
@@ -105,60 +112,167 @@ local function main()
     --Receiving rules
     local m,err = ngx.re.match(ngx.var.request_uri,"^/guanzhu/([^/]+)/weixin/rules","jo")
     local method = ngx.req.get_method()
-    if m and method == "POST"  then
+    if m and ( method == "POST" or method == "GET" or method == "DELETE") then
         log(ngx.INFO,"received rules!!!")
-        local data = get_client_body()
-        if not data then
-            log(ngx.ERR,"err rule no body data")
+        local rule_id = ngx.var.arg_id
+        if not rule_id then
+            log(ngx.ERR,"Parameter error")
             local t = {}
             t.code = 403
             t.status = "fail"
-            t.message = "rule no body data"
+            t.message = "Parameter error"
             ngx.send_headers()    
             ngx.print(cjson.encode(t))
             ngx.flush(true)
             return 
         end
+        if method == "POST" then --下发规则
+            local data = get_client_body()
+            if not data then
+                log(ngx.ERR,"err rule no body data")
+                local t = {}
+                t.code = 403
+                t.status = "fail"
+                t.message = "rule no body data"
+                ngx.send_headers()    
+                ngx.print(cjson.encode(t))
+                ngx.flush(true)
+                return 
+            end
+            local t = cjson.decode(data)
+            local sex = t.sex
+            local place = t.place
+            local priority = t.priority
+            local interest = t.interest
+            local guanzhu = t.guanzhu
+    
+            --保存规则
+            local resp, err =  redis_cli:hmset("rule" .. rule_id,"id",rule_id,"sex",sex,"place",place,"priority",priority,"interest",interest,"guanzhu",guanzhu)
+            if not resp then  
+                log(ngx.ERR,"hmset ",err) 
+                return
+            end 
+    
+            --保存规则的key
+            local resp, err =  redis_cli:sadd("all_rule","rule" .. rule_id)
+            if not resp then  
+                log(ngx.ERR,"sadd ",err) 
+                return
+            end 
+    
+            --将待处理的规则的key入队列
+            local resp, err =  redis_cli:rpush("rule_queue","rule" .. rule_id)
+            if not resp then  
+                log(ngx.ERR,"rpush",err) 
+                return
+            end 
+    
+            local t = {}
+            t.code = 200
+            t.messsage = "send rule success"
+            t.status = "success"
+            ngx.send_headers()    
+            ngx.print(cjson.encode(t))
+            ngx.flush(true)
+            return
+        end
         
-        local t = cjson.decode(data)
-        local rule_id = t.id
-        local sex = t.sex
-        local place = t.place
-        local priority = t.priority
-        local interest = t.interest
-        local guanzhu = t.guanzhu
+        if method == "GET" then --查询规则当前是否处理完成
+            local match_count, err = redis_cli:get("rule" .. rule_id .. "match_count")
+            local resp1, err1 =  redis_cli:sismember("completed_rule","rule" .. rule_id)
+            local resp2, err2 =  redis_cli:exists("rule" .. rule_id)
 
-        --保存规则
-        local resp, err =  redis_cli:hmset("rule" .. rule_id,"id",rule_id,"sex",sex,"place",place,"priority",priority,"interest",interest,"guanzhu",guanzhu)
-        if not resp then  
-            log(ngx.ERR,"err rule hmset") 
+            local t = {}
+            t.code = 200
+            t.status = "success"
+            if resp2 ~= 1 then  --查询规则是否存在
+                t.messsage = "non-existent"
+                t.match_count = 0
+            elseif resp1 ~= 1 then --查询规则是否匹配完成
+                t.messsage = "unfinished"
+                t.match_count = 0
+            else
+                t.match_count = match_count
+                t.messsage = "finished"
+            end
+
+            ngx.send_headers()    
+            ngx.print(cjson.encode(t))
+            ngx.flush(true)
             return
-        end 
+        end
 
-        --保存规则的key
-        local resp, err =  redis_cli:sadd("all_rule","rule" .. rule_id)
-        if not resp then  
-            log(ngx.ERR,"err rule sadd") 
+        if method == "DELETE" then --删除规则
+            local resp, err =  redis_cli:exists("rule" .. rule_id)
+            if resp ~= 1 then  
+                local t = {}
+                t.code = 403
+                t.messsage = "no this rule"
+                t.status = "failed"
+                ngx.send_headers()    
+                ngx.print(cjson.encode(t))
+                ngx.flush(true)
+                return
+            end
+
+            redis_cli:del("rule" .. rule_id)
+            dict:set("rule" .. rule_id,"deleted")
+
+            --将待处理的规则的key入队列
+            local resp, err =  redis_cli:rpush("rule_queue_del","rule" .. rule_id)
+            if not resp then  
+                log(ngx.ERR,"rpush",resp) 
+                return
+            end 
+    
+            local t = {}
+            t.code = 200
+            t.messsage = "del rule success"
+            t.status = "success"
+            ngx.send_headers()    
+            ngx.print(cjson.encode(t))
+            ngx.flush(true)
             return
-        end 
+        end
+    end
 
-        --将待处理的规则的key入队列
-        local resp, err =  redis_cli:rpush("rule_queue","rule" .. rule_id)
-        if not resp then  
-            log(ngx.ERR,"err rule rpush") 
-            return
-        end 
-
+    local m,err = ngx.re.match(ngx.var.request_uri,"^/version","jo")
+    local method = ngx.req.get_method()
+    if m and method == "GET"  then
+        local detail = ngx.var.arg_detail
         local t = {}
         t.code = 200
-        t.messsage = "send rule success"
         t.status = "success"
+        t.data = {}
+        t.data.service_name = "guanzhu"
+        t.data.version = "1.0.0"
+        
+        if detail == "yes" then
+            t.data.ip = dict:get("guanzhu_ip")
+            t.data.port = 80
+        end
+        
         ngx.send_headers()    
         ngx.print(cjson.encode(t))
         ngx.flush(true)
         return
     end
-    
+
+    local m,err = ngx.re.match(ngx.var.request_uri,"^/guanzhu/([^/]+)/status","jo")
+    local method = ngx.req.get_method()
+    if m and method == "GET" then
+        local t = {}
+        t.code = 200
+        t.status = "success"
+        t.data = {}
+        t.data.run = "ok"
+        ngx.send_headers()    
+        ngx.send_headers()    
+        ngx.print(cjson.encode(t))
+        ngx.flush(true)
+        return
+    end
+
     log(ngx.ERR,"wrong url:",ngx.var.request_uri)
     local t = {}
     t.code = 403
--- docker/gunzhu/lua/guanzhu_timer.lua
+++ docker/gunzhu/lua/guanzhu_timer.lua
@@ -1,28 +1,44 @@
 local log = ngx.log
-local ERR = ngx.ERR
 local new_timer = ngx.timer.at
 local dict = ngx.shared.dict
-local redis = require "resty.redis" 
-local cjson = require "cjson"
+local redis = require "resty.redis-util" --https://github.com/anjia0532/lua-resty-redis-util
+
+---去掉文件名后缀
+function getFileName(str)
+    local idx = str:match(".+()%.%w+$")
+    if(idx) then
+        return str:sub(1, idx-1)
+    else
+        return str
+    end
+end
+
+---获得文件扩展名
+function getExtension(str)
+    return str:match(".+%.(%w+)$")
+end
+
+--获得rootpath下以gz为扩展名的文件列表
+function getpaths(rootpath)
+    pathes =  {}
+    local file = io.popen("ls " .. rootpath)
+    if file then  
+        for entry in file:lines() do
+            if getExtension(entry) == "gz" and string.find(entry,"ak_user_tag",1,true) then
+                table.insert(pathes, entry)
+            end
+        end
+    end
+    return pathes
+end
 
 --分割字符串
-function split(pString, pPattern)
-   local Table = {}  -- NOTE: use {n = 0} in Lua-5.0
-   local fpat = "(.-)" .. pPattern
-   local last_end = 1
-   local s, e, cap = pString:find(fpat, 1)
-   while s do
-      if s ~= 1 or cap ~= "" then
-     table.insert(Table,cap)
-      end
-      last_end = e+1
-      s, e, cap = pString:find(fpat, last_end)
-   end
-   if last_end <= #pString then
-      cap = pString:sub(last_end)
-      table.insert(Table, cap)
-   end
-   return Table
+function split( str,reps )
+    local resultStrList = {}
+    string.gsub(str,'[^'..reps..']+',function ( w )
+        table.insert(resultStrList,w)
+    end)
+    return resultStrList
 end
 
 local function getip(service_name)
@@ -35,12 +51,14 @@ end
 
 local function up_ips()
     local redis_ip = getip("REDIS-SERVER")
+    local guanzhu_ip = getip("GUANZHU")
     dict:set("redis_ip",redis_ip)
-    log(ngx.INFO,"redis_ip:",redis_ip)
+    dict:set("guanzhu_ip",guanzhu_ip)
+    log(ngx.INFO,"redis_ip:",redis_ip," guanzhu_ip:",guanzhu_ip)
 end
 
- 
-update_ips_fun = function(premature)
+--更新redis-server的ip地址
+local function update_ips_fun(premature)
     if not premature then
         log(ngx.INFO,"up_ips....")
         up_ips()
@@ -52,117 +70,92 @@ update_ips_fun = function(premature)
     end
 end
 
+--开始匹配规则
 local function process_rules()
 
-	--连接redis server
-    local redis_cli = redis.new() 
-    redis_cli:set_timeout(60000)
-    local ok, err = redis_cli:connect(dict:get("redis_ip"), 6379) 
-    if not ok then 
-        log(ngx.ERR,"connect redis error:",err)
-        return 
-    end 
-    --local red, err = redis_cli:auth("foobared")
-    --if not red then
-    --    log(ngx.ERR,"failed to authenticate: ", err)
-    --    return
-    --end
+    local redis_cli = redis:new({ host=dict:get("redis_ip") });
 
-	--获取当前使用的目录
-	local dir, err = redis_cli:get("last-used-dir")
-	if not dir then
-		log(ngx.ERR,"err get") 
-		return
-	end 
-
-	if dir == ngx.null then --表示当前日志还没有被切分
-		log(ngx.NOTICE,"LOG is not splited") 
-		return
-	end 
-
-	--从队列中获取一个待处理的规则,获取的是规则key
-    local rule_key, err = redis_cli:lpop("rule_queue")
-
-    if not rule_key then  
-        log(ngx.ERR,"err lpop") 
+    --获取当前使用的目录
+    local dir, err = redis_cli:get("last-used-dir")
+    if not dir then
+        log(ngx.INFO,"LOG is not splited,wait..") 
         return
     end 
 
-    if rule_key ~= ngx.null then
-        local resp, err = redis_cli:hgetall(rule_key)--根据key获取规则内容
-        if not resp then  
-            log(ngx.ERR,"err hgetall") 
-            return
-        end 
+    --从队列中获取一个待处理的规则,获取的是规则key
+    local rule_key, err = redis_cli:lpop("rule_queue")
+    if not rule_key then  
+        log(ngx.INFO,"no rule is ready to process") 
+        return
+    end 
 
-		local count = 0
-        local wait_to_redis = {}
-        local rule = redis_cli:array_to_hash(resp)
-		local places = split(rule.place,',')
-		
-		for i=1,#places do
-			local province_city = split(places[i],'-')
-			file = io.open("/home/bigdata/" .. dir .."/" .. province_city[1] ..".txt","r");
-			if file then  
-				for line in file:lines() do
-					local t = split(line,'\t')
-					local begin_sex, end_sex =string.find(rule.sex,t[2],1,true)--首先匹配性别
-					if begin_sex then  
-						local begin_place, end_place =string.find(rule.place,t[3] .. "-" .. t[4],1,true)--匹配省份城市
-						if begin_place then  
-							local love_score = split(t[6],',')--分割兴趣爱好
-							for k=1, #love_score do  --匹配每个爱好
-								local love = split(love_score[k],':')--去掉每个爱好后面的分数
-								local begin_love, ends_love =string.find(rule.interest,love[1],1,true)
-								if begin_love then  
-									local zdd_arg = {}
-									zdd_arg.arg1 = t[1] .. "guanzhu"
-									zdd_arg.arg2 = rule.priority
-									zdd_arg.arg3 = rule.guanzhu
-									table.insert(wait_to_redis,zdd_arg)
-									count = count + 1
-									break--匹配到一个就可以把公众号加入到该mac
-								end
-							end 
-						end
-					end
-					
-					if count >= 1000 then
-						redis_cli:init_pipeline()
-						for j=1, #wait_to_redis do  --匹配每个爱好
-							redis_cli:zadd(wait_to_redis[j].arg1,wait_to_redis[j].arg2,wait_to_redis[j].arg3)
-						end
-						redis_cli:commit_pipeline()
-						wait_to_redis = {}
-						count = 0
-					end
-				end
-				file:close()
-			end 
-		end
-        
+    --根据key获取规则内容
+    local resp, err = redis_cli:hgetall(rule_key)
+    if not resp then  
+        log(ngx.INFO,"key has been deleted ",rule_key) 
+        return
+    end 
 
-        if count > 0 then
-            redis_cli:init_pipeline()
-            for i=1, #wait_to_redis do 
-                redis_cli:zadd(wait_to_redis[i].arg1,wait_to_redis[i].arg2,wait_to_redis[i].arg3)
-            end
-            redis_cli:commit_pipeline()
-            wait_to_redis = {}
-            count = 0
+    local count = 0
+    local total = 0
+    local rule = redis_cli:array_to_hash(resp)
+    local places = split(rule.place,',')
+    
+    for i=1,#places do
+        local grep
+        if rule.interest == "不限"  then
+            grep = string.format([[cat %s/%s]],dir,places[i])
+        else
+            grep = string.format([[grep -E "%s" %s/%s]],string.gsub(rule.interest,",","|"),dir,places[i])--根据省份，去匹配相应的文件
         end
 
-        --保存已完成规则的key
-        local resp, err =  redis_cli:sadd("completed_rule",rule_key)
-        if not resp then  
-            log(ngx.ERR,"err rule sadd") 
-            return
-        end 
-        log(ngx.NOTICE,"completed_rule ",rule_key) 
-    end 
+        if rule.sex == "不限" then
+            grep = string.format([[%s | awk 'BEGIN{FS="\t"} {print $1}']],grep)--输出匹配的mac
+        else
+            grep = string.format([[%s | awk 'BEGIN{FS="\t"} {if($2=="%s"){ print $1 } }']],grep,rule.sex)
+        end
+
+        local file = io.popen(grep)
+        if file then  
+            for mac in file:lines() do
+                if count == 0 then
+                    redis_cli:init_pipeline()
+                    redis_cli:zadd(mac .. "guanzhu",rule.priority,rule_key .. "," .. rule.guanzhu)
+                    count = count + 1
+                elseif count >= 500 then
+                    redis_cli:commit_pipeline()
+                    total = total + count
+                    count = 0
+                    
+                    if dict:get(rule_key) == "deleted" then
+                        redis_cli:sadd("completed_rule",rule_key)
+                        dict:set(rule_key,nil)
+                        return
+                    end
+                else
+                    redis_cli:zadd(mac .. "guanzhu",rule.priority,rule_key .. "," .. rule.guanzhu)
+                    count = count + 1
+                end
+            end
+            file:close()
+        end
+    end
+
+    if count > 0 then
+        redis_cli:commit_pipeline()
+        total = total + count
+        count = 0
+    end
+
+    redis_cli:init_pipeline()
+    redis_cli:set(rule_key .. "match_count",total)--保存该规则匹配到的mac数量
+    redis_cli:sadd("completed_rule",rule_key)--保存已完成匹配的规则的key
+    redis_cli:commit_pipeline()
+
+    log(ngx.NOTICE,"completed_rule ",rule_key) 
 end
 
-process_rules_fun = function(premature)
+local function process_rules_fun(premature)
     if not premature then
         log(ngx.INFO,"timer process_rules....")
         process_rules()
@@ -176,88 +169,105 @@ end
  
 local function split_file()
 
-    ori_file = io.open("/home/bigdata/1.txt" ,"r");--先打开顺网文件,确保文件存在
-    if not ori_file then  
-        log(ngx.ERR,"open /home/bigdata/1.txt failed") 
-		return
-    end 
-	
-    local redis_cli = redis.new() 
-    redis_cli:set_timeout(60000)
-    local ok, err = redis_cli:connect(dict:get("redis_ip"), 6379) 
-    if not ok then 
-        log(ngx.ERR,"connect redis error:",err)
-        return 
-    end 
-    --local red, err = redis_cli:auth("foobared")
-    --if not red then
-    --    log(ngx.ERR,"failed to authenticate: ", err)
-    --    return
-    --end
- 
-    local resp, err = redis_cli:get("last-modified")--获取顺网数据最后一次修改时间
-    if not resp then  
-        log(ngx.ERR,"err get") 
+    local rootpath = "/home/bigdata/shunwang"
+    local paths = getpaths(rootpath)--获取目录下所有以gz为扩展名的文件
+
+    if #paths == 0 then
         return
-    end 
+    end
 
-	local stat = io.popen("stat -c %Y /home/bigdata/1.txt")--获取顺网数据最后一次修改时间
-	local last_modified = stat:read()
-	if resp ~= last_modified then--需要重新分割文件
+    local changtime = 0
+    local filename = ""
+    for i=1,#paths do
+        local stat = io.popen("stat -c %Y " .. rootpath .. "/" .. paths[i])--获取修改时间
+        local last_modified = tonumber(stat:read())
+        if last_modified > changtime then
+            changtime = last_modified
+            filename = paths[i]
+        end
+    end
+    
+    local redis_cli = redis:new({ host=dict:get("redis_ip") });
 
-		local resp, err = redis_cli:get("last-used-dir")--获取当前使用的目录
-		if not resp then  
-			log(ngx.ERR,"err get") 
-			return
-		end 
-		
-		local dir = ""
-		if resp ~= "split1" then --/home/bigdata/split1/江苏省.txt 北京市.txt
-			dir = "split1"
-		else
-			dir = "split2"
-		end
-		
-		local cmd_rm = os.execute("rm /home/bigdata/" .. dir .. "/*.txt")--先删除该目录下所有文件
-		
-		local province_files = {}
-		for line in ori_file:lines() do
-			local t = split(line,'\t')
-			
-			if not province_files[t[3]] then--打开相应省份的txt文件
-			    tmpfile = io.open("/home/bigdata/" .. dir .."/" .. t[3] ..".txt" ,"w+");
-				if not tmpfile then  
-					log(ngx.ERR,"/home/bigdata/" .. dir .."/" .. t[3] ..".txt") 
-					return
-				end 
-				province_files[t[3]] = tmpfile
-			end
-			
-			province_files[t[3]]:write(line .. "\n")
-		end
-		
-		local resp, err = redis_cli:set("last-used-dir",dir)--获取当前使用的目录
-		if not resp then  
-			log(ngx.ERR,"err set") 
-			return
-		end 
-		
-		local resp, err = redis_cli:set("last-modified",last_modified)
-		if not resp then  
-			log(ngx.ERR,"err set") 
-			return
-		end 
-		
-		for i=1,#province_files do
-			province_files[i]:close()
-		end
-	end
+    local resp, err = redis_cli:get("changtime" .. filename)--时间在变化，说明文件还在传输
+    if not resp or tonumber(resp) ~= changtime then
+        redis_cli:set("changtime" .. filename,changtime)
+        return
+    end
+
+    log(ngx.NOTICE,"spliting begin.................") 
+    local dir = ""
+    local resp, err = redis_cli:get("last-used-dir")--获取当前使用的目录
+    if resp ~= "/home/bigdata/split1" then --/home/bigdata/split1/江苏-南京.txt 北京-北京.txt
+        dir = "/home/bigdata/split1"
+    else
+        dir = "/home/bigdata/split2"
+    end
+
+    os.execute(string.format([[rm -f %s/*]],dir))--删除该目录下所有文件
+
+    local result = os.execute(string.format([[cd %s && gunzip %s/%s]],rootpath,rootpath,filename))--解压
+    if not result then
+        log(ngx.ERR, "failed to gunzip ", filename)
+        os.execute(string.format([[rm -f %s/%s]],rootpath,filename))
+        os.execute(string.format([[rm -f %s/%s]],rootpath,getFileName(filename)))
+        return
+    end
+
+    os.execute(string.format([[mv %s/%s %s/全国]],rootpath,getFileName(filename),dir))
+    os.execute(string.format([[ awk 'BEGIN{FS="\t"} {if($3!="\\N" && $4!="\\N"){print $0 >> "%s/"$3"-"$4} if($3!="\\N"){print $0 >> "%s/"$3}}' %s/全国]],dir,dir,dir))
+    
+    redis_cli:init_pipeline()
+    redis_cli:set("last-used-dir",dir)
+    redis_cli:del("changtime" .. filename)
+    redis_cli:commit_pipeline()
+    log(ngx.NOTICE,"spliting end")
 end
-split_file_fun = function(premature)
+
+local function split_file_fun(premature)
     if not premature then
         log(ngx.INFO,"timer split_file_fun....")
         split_file()
-        local ok, err = new_timer(60, split_file_fun)
+        local ok, err = new_timer(10, split_file_fun)
+        if not ok then
+            log(ngx.ERR, "failed to create timer: ", err)
+            return
+        end
+    end
+end
+
+local function process_del_rules()
+    --连接redis server
+    local redis_cli = redis:new({ host=dict:get("redis_ip") });
+
+    --从队列中获取一个待处理的规则,获取的是规则key
+    local rule_key, err = redis_cli:lindex("rule_queue_del",0)
+
+    if not rule_key then  
+        log(ngx.INFO,"no rule is ready to process") 
+        return
+    end 
+
+    local resp, err =  redis_cli:sismember("completed_rule",rule_key)
+    if resp ~= 1 then  
+        log(ngx.NOTICE,"wait to del") 
+        return
+    end
+
+    redis_cli:init_pipeline()
+    redis_cli:del(rule_key .. "match_count")
+    redis_cli:srem("all_rule",rule_key)
+    redis_cli:srem("completed_rule",rule_key)
+    redis_cli:lpop("rule_queue_del")
+    redis_cli:commit_pipeline()
+    log(ngx.NOTICE,"completed delete rule ",rule_key) 
+
+end
+local function process_del_rules_fun(premature)
+    if not premature then
+        log(ngx.INFO,"timer process_del_rules_fun....")
+        process_del_rules()
+        local ok, err = new_timer(5, process_del_rules_fun)
         if not ok then
             log(ngx.ERR, "failed to create timer: ", err)
             return
@@ -268,19 +278,30 @@ end
 local function main()
 
     if 0 == ngx.worker.id() then
-	    --更新redis server ip地址
-        local ok, err = new_timer(1, update_ips_fun)
+    
+        --切分文件时需要，假设正在读取split1下文件，那么顺网更新数据时，将新切分的文件放在split2下
+        os.execute("mkdir /home/bigdata/split1")
+        os.execute("mkdir /home/bigdata/split2")
+        --更新redis server ip地址
+        local ok, err = new_timer(0, update_ips_fun)
         if not ok then
             ngx.log(ngx.ERR, "failed to create a timer: ", err)
             return
         end
-		
-		--按省份切割顺网给的原始数据文件
-		local ok, err = new_timer(2, split_file_fun)
-		if not ok then
-			ngx.log(ngx.ERR, "failed to create a timer: ", err)
-			return
-		end
+        
+        --按省份切割顺网给的原始数据文件
+        local ok, err = new_timer(2, split_file_fun)
+        if not ok then
+            ngx.log(ngx.ERR, "failed to create a timer: ", err)
+            return
+        end
+        
+            --删除广告商下发的规则
+        local ok, err = new_timer(5, process_del_rules_fun)
+        if not ok then
+            ngx.log(ngx.ERR, "failed to create a timer: ", err)
+            return
+        end
     end
 
     --处理广告商下发的规则
@@ -292,4 +313,3 @@ local function main()
 end
 
 main()
- 
